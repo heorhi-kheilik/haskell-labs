2bf7d4a9e3a6101574e519f5c0c0871c89208e02
Heorhi Heilik <heheilik@icloud.com>
2024-10-18 11:24:33 +0300

    multiplication implemented

--- 04-03-fibonacci.hs
+++ 04-03-fibonacci.hs
@@ -1,20 +1,3 @@
--- [
---     [1, 2, 3],
---     [4, 5, 6]
--- ]
-
--- [
---     [1, 2],
---     [3, 4],
---     [5, 6]
--- ]
-
--- matrix_mul :: Num a => [[a]] -> [[a]] -> [[a]]
--- matrix_mul m_first m_second = let i = [0..length matrix]
---                                   j = [0..length matrix !! 0] in
-
--- matrix_calc_mul_upper_left matrix = 
-
 vector_mul [] [] acc = acc
 vector_mul v_first v_second acc = vector_mul tail_v_first tail_v_second new_acc
     where new_acc                       = head_v_first * head_v_second + acc
@@ -24,26 +7,18 @@ vector_mul v_first v_second acc = vector_mul tail_v_first tail_v_second new_acc
 matrix_mul_by_col :: Num a => [[a]] -> [a] -> [a]
 matrix_mul_by_col matrix col = [ vector_mul row col 0 | row <- matrix ]
 
-matrix_add_col matrix col = [ current_row ++ [col_elem] | (current_row, col_elem) <- zip matrix col ]
+matrix_push_last_col matrix col = [ current_row ++ [col_elem] | (current_row, col_elem) <- zip matrix col ]
 
+matrix_pop_first_col :: Num a => [[a]] -> ([a], [[a]])
+matrix_pop_first_col matrix
+    | all null matrix   = ([], matrix)
+    | otherwise         = unzip [ (elem, row) | (elem : row) <- matrix ]
 
+matrix_mul m_first m_second = matrix_mul_helper m_first m_second (take (length m_first) $ repeat [])
 
-
--- matrix_drop_row :: Num a => [[a]] -> [[a]]
--- matrix_drop_row (row:other) = other
-
--- matrix_drop_col :: Num a => [[a]] -> [[a]]
--- matrix_drop_col matrix = [ tail row | row <- matrix ]
-
-
-
-
--- matrix_calc_mul_element :: Num a, Integral b => [a] -> b -> b -> a
--- matrix_calc_mul_element matrix i j = sum [ x * y | (x, y) <- zip row col ]
---     where row = matrix !! i
---           col = get_col matrix j
---           get_col matrix index = [ row !! index | row <- matrix ]
-
--- matrix_get_row matrix index = matrix !! index
-
--- matrix_get_col matrix index = [ row !! index | row <- matrix ]
+matrix_mul_helper m_first m_second m_acc
+    | all null m_second     = m_acc
+    | otherwise             = matrix_mul_helper m_first cropped m_new_acc
+        where m_new_acc          = matrix_push_last_col m_acc res_col
+              res_col            = matrix_mul_by_col m_first cur_col
+              (cur_col, cropped) = matrix_pop_first_col m_second

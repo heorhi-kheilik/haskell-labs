55e9a9b868c8382cceff7962f20e562761fb53bd
Heorhi Heilik <heheilik@icloud.com>
2024-11-11 00:04:53 +0300

    07 + sieve

--- 06/06-07-unfold-to-prime.hs
+++ 06/06-07-unfold-to-prime.hs
@@ -1,11 +1,15 @@
 import Data.List;
 
+unfoldToPrimeWithSieve limit = unfoldr _unfoldToPrimeWithSieveH [2..limit]
+
+_unfoldToPrimeWithSieveH [] = Nothing
+_unfoldToPrimeWithSieveH (current : sieveSuffix) =
+    let newSieveSuffix = filter (\x -> mod x current /= 0) sieveSuffix in Just (current, newSieveSuffix)
+
 unfoldToPrime = (2 : unfoldr _unfoldToPrimeH ([2], 3))
 
-_unfoldToPrimeH (sieve, current) = let new = _newSieveElement sieve current in Just (new, (sieve ++ [new], new + 1))
+_unfoldToPrimeH (primes, current) = let new = _newPrimeElement primes current in Just (new, (primes ++ [new], new + 1))
 
-_newSieveElement sieve current = if null $ filter id [ mod current sElement == 0 | sElement <- sieve ]
+_newPrimeElement primes current = if null $ filter id [ mod current prime == 0 | prime <- primes ]
     then current
-    else _newSieveElement sieve (current + 1)
-
-unfoldToPrimeNotAbove n = takeWhile (\x -> x <= n) unfoldToPrime
+    else _newPrimeElement primes (current + 1)

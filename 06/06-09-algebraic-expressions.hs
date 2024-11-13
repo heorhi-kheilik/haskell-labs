88d7c0a23e1684ea6ed05631e456314c70112b0d
Heorhi Heilik <heheilik@icloud.com>
2024-11-13 11:55:25 +0300

    09 not completed

---
+++ 06/06-09-algebraic-expressions.hs
@@ -0,0 +1,88 @@
+data UnaryOperation = Negate | Sqrt | Sin | Cos | Tan
+
+instance Show UnaryOperation where
+    show Negate = "-$0"
+    show Sqrt = "sqrt($0)"
+    show Sin = "sin($0)"
+    show Cos = "cos($0)"
+
+applyUnary :: UnaryOperation -> Double -> Double
+applyUnary op num = case op of
+    Negate  -> -num
+    Sqrt    -> sqrt num
+    Sin     -> sin num
+    Cos     -> cos num
+    Tan     -> tan num
+
+
+data BinaryOperation = Add | Subtract | Multiply | Divide | Min | Max
+
+instance Show BinaryOperation where
+    show Add = "$0 + $1"
+    show Subtract = "$0 - $1"
+    show Multiply = "$0 * $1"
+    show Divide = "$0 / $1"
+    show Min = "min($0, $1)"
+    show Max = "max($0, $1)"
+
+applyBinary :: BinaryOperation -> Double -> Double -> Double
+applyBinary op num1 num2 = case op of
+    Add         -> num1 + num2
+    Subtract    -> num1 - num2
+    Multiply    -> num1 * num2
+    Divide      -> num1 / num2
+    Min         -> min num1 num2
+    Max         -> max num1 num2
+
+
+data AlgebraicTree
+    = ATVertexU UnaryOperation AlgebraicTree
+    | ATVertexB BinaryOperation AlgebraicTree AlgebraicTree
+    | ATLeafC Double
+    | ATLeafV String
+
+instance Show AlgebraicTree where
+    show tree = case tree of
+        ATLeafC const               -> show const
+        ATLeafV var                 -> show var
+        ATVertexU op tree           -> _format (show op) [tree]
+        ATVertexB op tree1 tree2    -> "(" ++ _format (show op) [tree1, tree2] ++ ")"
+
+_format :: Show a => String -> [a] -> String
+_format template args = let (first, second) = break (\x -> x == '$') template in
+    if null second
+        then template
+        else
+            let
+                argStr = show $ args !! argIndex
+                (_ : argIndexStr : secondSuffix) = second
+                argIndex :: Int = read [argIndexStr]
+                formatted = first ++ argStr ++ secondSuffix
+            in
+                _format formatted args
+
+
+solve :: AlgebraicTree -> Double
+solve t = case t of
+    ATLeafC const               -> const
+    ATLeafV mapping             -> 0
+    ATVertexU op tree           -> applyUnary op (solve tree)
+    ATVertexB op tree1 tree2    -> applyBinary op (solve tree1) (solve tree2)
+
+-- simplify :: AlgebraicTree -> AlgebraicTree
+-- simplify tree = case tree of
+--     ATLeafC _                                           -> tree
+--     ATLeafV _                                           -> tree
+--     ATVertexU _ (ATLeafC _)                             -> ATLeafC (solve tree)
+--     ATVertexB _ (ATLeafC _) (ATLeafC _)                 -> ATLeafC (solve tree)
+--     ATVertexU Negate (ATVertexU Negate (ATLeafV var))   -> ATLeafV var
+--     ATVertexB Add opSin opCos
+--     ATVertexB Add (
+--         ATVertexB Multiply
+--             (ATVertexU Sin (ATLeafV var1))
+--             (ATVertexU Sin (ATLeafV var2))
+--     ) (
+--         ATVertexB Multiply
+--             (ATVertexU Cos (ATLeafV var3))
+--             (ATVertexU Cos (ATLeafV var4))
+--     )                                                   -> ATLeafC 1
